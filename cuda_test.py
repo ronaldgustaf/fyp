bb40518abbde3aa9889ab2d41d28eb24b90ab519
ronaldgustaf <ronaldgustaf@gmail.com>
2024-11-14 18:49:31 +0800

    add cuda test

--- cuda_test.py
+++ cuda_test.py
@@ -12,18 +12,3 @@ if device.type == 'cuda':
     print('Memory Usage:')
     print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
     print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
-    
-    
-# import transformers
-# import torch
-
-# model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
-
-# pipeline = transformers.pipeline(
-#   "text-generation",
-#   model="meta-llama/Meta-Llama-3.1-8B-Instruct",
-#   model_kwargs={"torch_dtype": torch.bfloat16},
-#   device="cuda",
-# )
-
-# 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
